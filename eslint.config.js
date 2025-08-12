fa9d9b48f54820bd81c1d3d9c675770b555c0dec
Tracy Kabuya <tracykabuya@gmail.com>
2025-08-13 00:01:11 +0200

    created the navigation bar, with the use of React Router

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // Ignore certains dossiers
+  globalIgnores(['dist', 'node_modules']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.recommended, // Règles React de base
       reactHooks.configs['recommended-latest'],
+      jsxA11y.configs.recommended, // Accessibilité
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
     rules: {
+      ...prettier.configs.recommended.rules, // Active Prettier via ESLint
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off', // Désactive prop-types si inutile
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-])
+]);
