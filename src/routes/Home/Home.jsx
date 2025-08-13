a5c3e79890160d8e176a49432f99e0a78f21cae0
Tracy Kabuya <tracykabuya@gmail.com>
2025-08-13 17:26:56 +0200

    hamburger menu created for responsive navbar

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,52 @@
-import { Outlet } from 'react-router-dom'
+import image1 from '../../assets/image1.jpg';
+import image2 from '../../assets/image2.jpg';
+import image3 from '../../assets/image3.jpg';
+import image4 from '../../assets/image4.jpg';
+import image5 from '../../assets/image5.jpg';
+import image6 from '../../assets/image6.jpg';
+
+import './Home.scss'
 
 const Home = () => {
+  const projects = [
+    {
+      "id": 1,
+      "title": "projet1",
+      "imageUrl": image1
+    },
+    {
+      "id": 2,
+      "title": "projet2",
+      "imageUrl": image2
+    },
+    {
+      "id": 3,
+      "title": "projet3",
+      "imageUrl": image3
+    },
+    {
+      "id": 4,
+      "title": "projet4",
+      "imageUrl": image4
+    },
+    {
+      "id": 5,
+      "title": "projet5",
+      "imageUrl": image5
+    },
+    {
+      "id": 6,
+      "title": "projet6",
+      "imageUrl": image6
+    }
+  ]
+
+
+
+
   return (
     <div>
-      <h1>Home</h1>
-      <Outlet />
+      <CardList />
     </div>
   )
 }
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,28 +1,40 @@
-import { Fragment } from 'react';
+import { useState, Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import LogoEspaceConstruction from '../../assets/logo-espace-construction.svg?react';
 import './Navigation.scss'
 
 
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
           <LogoEspaceConstruction className="logo" />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to='/'>
+        {/* Bouton hamburger (visible seulement sur mobile) */}
+        <div
+          className={`hamburger ${isMenuOpen ? 'active' : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Liens navigation */}
+        <div className={`nav-links-container ${isMenuOpen ? 'open' : ''}`}>
+          <Link className="nav-link" to="/" onClick={() => setIsMenuOpen(false)}>
             References
           </Link>
-          <Link className="nav-link" to='/contact'>
+          <Link className="nav-link" to="/contact" onClick={() => setIsMenuOpen(false)}>
             Contact
           </Link>
         </div>
       </div>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
 export default Navigation
