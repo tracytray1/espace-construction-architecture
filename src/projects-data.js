7fe4fca431755d061b3cc0a6f229938c888bbb3b
Tracy Kabuya <tracykabuya@gmail.com>
2025-08-25 18:58:51 +0200

    optimized the rendering of the images, allowing them to display all at once

--- src/projects-data.js
+++ src/projects-data.js
@@ -1,40 +1,213 @@
-import image1 from './assets/image1.jpg';
-import image2 from './assets/image2.jpg';
-import image3 from './assets/image3.jpg';
-import image4 from './assets/image4.jpg';
-import image5 from './assets/image5.jpg';
-import image6 from './assets/image6.jpg';
+import collectifColonelFabien1 from './assets/homepage/collectif-colonel-fabien-dammarie-les-lys-1.jpg';
+import collectifColonelFabien2 from './assets/homepage/collectif-colonel-fabien-dammarie-les-lys-2.jpg';
+import collectifMarechalFoch from './assets/homepage/collectif-marechal-foch-dammarie-les-lys.jpg';
+import gitesFonrose from './assets/homepage/gites-fonrose-marie-galante.jpg';
+import immeubleChoisy from './assets/homepage/immeuble-choisy-alfortville.png';
+import immeubleBtb from './assets/homepage/immeuble-de-bureaux-btb-baie-mahault.jpg';
+import immeubleCerisiers from './assets/homepage/immeuble-des-cerisiers-thorigny-sur-marne.jpg';
+import immeubleDivisionLeclerc from './assets/homepage/immeuble-division-leclerc-le-bourget.jpg';
+import maisonAganahi from './assets/homepage/maison-aganahi-abomey-calavi-benin.png';
+import maisonDuclos from './assets/homepage/maison-duclos-douy-la-ramee.jpg';
+import maisonFayol from './assets/homepage/maison-fayol-dampmart.jpg';
+import maisonSalles from './assets/homepage/maison-salles-saint-maur-des-fosses.jpg';
+import maisonsChampsFleuris from './assets/homepage/maisons-champs-fleuris-chatillon.jpg';
+import pavillonPetitHavre from './assets/homepage/pavillon-petit-havre-le-gosier-guadeloupe.jpg';
+import villaBienDesire from './assets/homepage/villa-bien-desire-saint-francois-guadeloupe.jpg';
+import villaDeLutiis from './assets/homepage/villa-de-lutiis-thorigny-sur-marne.jpg';
+import villaIllemassene from './assets/homepage/villa-illemassene-bois-le-roi.jpg';
+import villaMahroum from './assets/homepage/villa-mahroum-andelu.jpg';
+import villasDesVarennes from './assets/homepage/villas-des-varennes-saint-germain-les-arpajons.jpg';
+
 
 export const PROJECTS_DATA =
  [
     {
       "id": 1,
-      "title": "projet1",
-      "imageUrl": image1
+      "title": "Collectif Colonel Fabien Dammarie lès Lys 1",
+      "imageUrl": collectifColonelFabien1,
+      "projet": "Collectif Colonel Fabien ",
+      "ville": "Dammarie-lès-Lys",
+      "texte": `En collaboration avec l’agence d’architecture ACSTETUDE, il s’agissait de réaliser
+      une étude d’avant-projet pour la conception d’un collectif horizontal dans un quartier résidentiel
+      de Dammarie-lès-Lys. Le terrain, profond, se développe en lanière perpendiculairement à la rue.
+
+    Les volumes bâtis s’organisent en trois ensembles : un premier groupe de logements en R+2+combles sur
+    rue, une série de duplex donnant sur une cour ouverte, et enfin un dernier groupe de logements superposés
+    à un parking en fond de parcelle. L’accès à la cour se fait par un passage couvert.
+
+    Ces masses bâties génèrent une forte intériorité, le projet étant protégé de la rue par le premier groupement.
+    Cette intériorité est renforcée aux extrémités de la cour par les retours et la surélévation des volumes.
+    Depuis la rue, le premier ensemble s’inscrit dans la continuité d’une succession de maisons de ville.
+
+    Les duplex de la partie centrale se situent à mi-chemin entre l’appartement et la maison de ville. Ils sont
+    orientés plein sud et bénéficient chacun d’un jardin privatif. Le séjour en double hauteur apporte lumière
+    et profondeur au cœur du logement.
+
+    Ces dessins présentent la première version du projet de collectif, dont le programme de logements a
+    malheureusement été rejeté par la mairie. Une seconde version est présentée sur ce site.`
     },
     {
       "id": 2,
-      "title": "projet2",
-      "imageUrl": image2
+      "title": "Collectif Colonel Fabien Dammarie lès Lys 2",
+      "imageUrl": collectifColonelFabien2,
+      "projet": "",
+      "ville": "",
+      "texte": ``
     },
     {
       "id": 3,
-      "title": "projet3",
-      "imageUrl": image3
+      "title": "Collectif Maréchal Foch Dammarie lès Lys",
+      "imageUrl": collectifMarechalFoch,
+      "projet": "",
+      "ville": "",
+      "texte": ``
     },
     {
       "id": 4,
-      "title": "projet4",
-      "imageUrl": image4
+      "title": "Gîtes Fonrose",
+      "imageUrl": gitesFonrose,
+      "projet": "",
+      "ville": "",
+      "texte": ``
     },
     {
       "id": 5,
-      "title": "projet5",
-      "imageUrl": image5
+      "title": "Immeuble Choisy Alfortville",
+      "imageUrl": immeubleChoisy,
+      "projet": "",
+      "ville": "",
+      "texte": ``
     },
     {
       "id": 6,
-      "title": "projet6",
-      "imageUrl": image6
-    }
+      "title": "Immeuble de bureaux Btb Baie Mahault",
+      "imageUrl": immeubleBtb,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 7,
+      "title": "Immeuble des Cerisiers Thorigny sur Marne",
+      "imageUrl": immeubleCerisiers,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 8,
+      "title": "Immeuble Division Leclerc Le Bourget",
+      "imageUrl": immeubleDivisionLeclerc,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 9,
+      "title": "Maison Aganahi Abomey Calavi",
+      "imageUrl": maisonAganahi,
+      "projet": "",
+      "ville": "",
+      "texte": `Le propriétaire d’un terrain situé dans une zone résidentielle d’Abomey-Calavi, au Bénin, nous a confié
+      l’étude d’un projet de maison individuelle. La mission s’est limitée à l’avant-projet, une équipe locale devant assurer
+      les dernières résolutions constructives ainsi que le suivi de chantier sur place.
+
+      La maison s’implante sur une parcelle
+      régulière et relativement plate, d’une profondeur de 30 m et d’une largeur de 10 m.Pour répondre à cette géométrie, le plan
+      du logement s’organise en trois carrés successifs de 10 m par 10 m.La partie centrale accueille le cœur du logement: l’espace
+      de vie en double hauteur, traversant et surplombé par une mezzanine.Côté rue, cet espace s’ouvre sur une terrasse extérieure
+      légèrement surélevée par rapport à la cour d’entrée, prolongeant la cuisine de manière à la fois spatiale et fonctionnelle.
+      De l’autre côté, il se prolonge vers une terrasse ouverte sur un jardin aux proportions équilibrées.
+
+      La transition intérieur/ extérieur est assurée par une façade épaisse conçue comme un filtre au vent, favorisant la ventilation
+      naturelle dans l’ensemble du logement.Des jalousies et ventelles en partie basse permettent de réguler les entrées d’air traversant
+      successivement le séjour et la cuisine dans le sens du vent.La façade sur rue est largement ouverte pour renforcer l’aspiration,
+      tandis qu’une écope de toiture en partie centrale conditionne également la circulation d’air depuis un point haut lorsque la cuisine
+      et l’entrée sont fermées.
+
+      Les réflexions liées à la conception bioclimatique du logement ont largement déterminé la coupe.L’étage est composé de deux volumes
+      décalés et espacés qui accueillent les chambres.Ce jeu de décalage, perçu côté rue comme l’élément structurant de la façade, confère
+      à l’ensemble une dynamique particulière.Les volumes traités en surplomb, associés au balcon et à la casquette, renforcent l’impression
+      d’un étage flottant au - dessus du rez - de - chaussée.`
+    },
+    {
+      "id": 10,
+      "title": "Maison Duclos Douy la Ramée",
+      "imageUrl": maisonDuclos,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 11,
+      "title": "Maison Fayol Dampmart",
+      "imageUrl": maisonFayol,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 12,
+      "title": "Maison Salles Saint-Maur-des-Fossés",
+      "imageUrl": maisonSalles,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 13,
+      "title": "Maisons Champs Fleuris Châtillon",
+      "imageUrl": maisonsChampsFleuris,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 14,
+      "title": "Pavillon Petit Havre Le Gosier Guadeloupe",
+      "imageUrl": pavillonPetitHavre,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 15,
+      "title": "Villa Bien Desire Saint François Guadeloupe",
+      "imageUrl": villaBienDesire,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 16,
+      "title": "Villa De Lutiis Thorigny-sur-Marne",
+      "imageUrl": villaDeLutiis,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 17,
+      "title": "Villa Illemassene Bois-Le-Roi",
+      "imageUrl": villaIllemassene,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 18,
+      "title": "Villa Mahroum Andalu",
+      "imageUrl": villaMahroum,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
+    {
+      "id": 19,
+      "title": "Villas Des Varennes Saint-Germain-Les-Arpajons",
+      "imageUrl": villasDesVarennes,
+      "projet": "",
+      "ville": "",
+      "texte": ``
+    },
   ]
